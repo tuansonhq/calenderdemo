15327ccabba05e4986427d3c890b81eedcafd6d1
tuansonhq <86151925+tuansonhq@users.noreply.github.com>
2022-02-24 15:39:37 +0700

    Update

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:demosql/db/db_helper.dart';
+import 'package:demosql/services/theme_services.dart';
+import 'package:demosql/ui/home_page.dart';
+import 'package:demosql/ui/theme.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+import 'package:get_storage/get_storage.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await DBHelper.initDb();
+  await GetStorage.init();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widgets is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: Themes.light,
+      darkTheme: Themes.dark,
+      themeMode: ThemeServices().theme,
+      home: HomePage(),
+    );
+  }
+}
+
